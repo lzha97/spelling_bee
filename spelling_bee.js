225113c69076c3e4b1596abc4590f7642d60382d
angsabrina <sabrina.ang@gatech.edu>
2019-11-20 17:27:43 -0500

    check pangram works, score reflects pangrams

--- spelling_bee.js
+++ spelling_bee.js
@@ -127,6 +127,7 @@ function submitWord(){
   var centerLetter = document.getElementById('center-letter').firstChild.innerHTML;
 
   let score = 0;
+  var isPangram = new Boolean(false);
   var showScore = document.getElementById("totalScore");
 
   if(tryword.value.length < 4){
@@ -136,7 +137,8 @@ function submitWord(){
   }else if(!tryword.value.toLowerCase().includes(centerLetter.toLowerCase())){
     alert('Word doesn\'t contain center letter!');
   }else if(validWords.includes(tryword.value.toLowerCase())){
-    addToTotalScore(calculateWordScore(tryword));
+    isPangram = checkPangram(tryword);
+    addToTotalScore(calculateWordScore(tryword, isPangram));
     showScore.innerHTML = totalScore;
     showDiscoveredWord(tryword);
     discoveredWords.push(tryword.value.toLowerCase());
@@ -186,6 +188,22 @@ function calculateWordScore(input, isPangram) {
     returnScore = len + 7;
   }
   return returnScore;
+}
+
+function checkPangram(input) {
+  var i;
+  var containsCount = 0;
+  var containsAllLetters = new Boolean(false);
+  for(i = 0; i < 7; i++) {
+    if(input.value.includes(letters[i])) {
+      containsCount++;
+    }
+  }
+  if(containsCount == 7) {
+    containsAllLetters = new Boolean(true);
+  }
+  console.log("isPangram?: " + containsAllLetters);
+  return containsAllLetters;
 }
 
 
