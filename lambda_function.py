23a0b6a8dd6bdbba43a0c1cf722d48f14495f7ba
Lillian Zha <lillianzha@gmail.com>
2019-11-21 20:52:07 -0500

    lambda: triggered everyday to generate letter set, returns letter set to http request

--- lambda_function.py
+++ lambda_function.py
@@ -1,27 +1,85 @@
 import json
+import random
+import boto3
+import os
+import logging
+from botocore.exceptions import ClientError
 
 def lambda_handler(event, context):
     # TODO implement
-
-    with open('validwords.json', 'r') as dictfile:
-        worddict = json.load(dictfile)
     
-    letters = event["queryStringParameters"]['letters']
-    print(letters)
+    print(event)
     
-    res = [] 
-    center_letter = letters[3]
+    ##for the daily generation of letters and their possible words
+    def is_pangram(string): 
+        if len(set(list(string))) == 7: return True 
+        return False
 
-    for w in worddict: 
-        flag = True
-        for c in w: 
-            if c not in letters: 
-                flag = False 
-        if center_letter not in w: 
-            flag = False 
-        if flag == True: res.append(w)
+    if 'detail-type' in event:
+        if event['detail-type'] == 'Scheduled Event':
+            worddict = dict()
+            with open('validwords.json', 'r') as dictfile:
+                worddict = json.load(dictfile)
+                pangram_set = set()
+                for k in worddict: 
+                    if is_pangram(k): 
+                        letterset = frozenset(k)
+                        pangram_set.add(letterset)
+                    
+            todays_letters = list(random.choice(list(pangram_set)))
+    
+            res = [] 
+            center_letter = todays_letters[3]
+            pangram = ''
+            for w in worddict: 
+                flag = True
+                for c in w: 
+                    if c not in todays_letters: 
+                        flag = False 
+                if center_letter not in w: 
+                    flag = False 
+                if flag == True:
+                    if is_pangram(w): pangram = w
+                    res.append(w)
+    
+            s3 = boto3.client('s3')
+            bucket_name = os.environ['BUCKET_NAME']
+            data = {'letters':todays_letters, 'pangram': pangram, 'possible_words':res, 'center_letter': center_letter}
+            key = os.environ['FILE_KEY']
+            response = s3.put_object(Bucket=bucket_name,Key=key,Body=json.dumps(data),ACL='public-read')
+            return response
 
-    return {
-        'statusCode': 200,
-        'body': json.dumps(res)
-    }
+    ## for the return of letters and details from the api request
+    if 'queryStringParameters' in event: 
+        
+        
+        bucket_name = os.environ['BUCKET_NAME']
+        key = os.environ['FILE_KEY']
+        s3 = boto3.resource('s3')
+        
+        try:
+            s3.Bucket(bucket_name).download_file(key, '/tmp/todays_letters.json')
+        except ClientError as e:
+            if e.response['Error']['Code'] == "404":
+                return {
+                            'statusCode': 404,
+                            'body': 'Oops! Could not find today\'s word file.'
+                        }
+                
+            else:
+                return {
+                            'statusCode': 500,
+                            'body': "Uh oh, an error occurred"
+                        }
+            
+        with open('/tmp/todays_letters.json', 'r') as rdfile: 
+            data = rdfile.read()
+            print(data)
+            data = json.loads(data)
+            return  {
+                        'statusCode': 200,
+                        'body': json.dumps(data, indent=4)
+                    }
+          
+        
+                
